dac6548343cd424de0870546307b9f71bd411cd7
Natanael-ADS <natanaelxhd7@gmail.com>
2022-01-18 09:25:57 -0300

    added flutter_module

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:muvver_app/modules/app_widget.dart';
+
+import 'modules/app_module.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Muvver',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Container());
-  }
+  runApp(ModularApp(
+    module: AppModule(),
+    child: AppWidget(),
+  ));
 }
---
+++ lib/modules/app_module.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  @override
+  List<Bind> get binds {
+    return [];
+  }
+
+  @override
+  List<ModularRoute> get routes {
+    return [];
+  }
+}
