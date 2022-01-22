3d45c72eecc133bae0536c0bd23fafba34c63faa
Natanael-ADS <natanaelxhd7@gmail.com>
2022-01-22 15:07:59 -0300

    dependency injection in module/traveler and tests

--- lib/modules/app_module.dart
+++ lib/modules/app_module.dart
@@ -2,6 +2,9 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:muvver_app/modules/transports/domain/usecase/search_transports.dart';
 import 'package:muvver_app/modules/transports/external/datasources/mock_sqlite_transport.dart';
 import 'package:muvver_app/modules/transports/infra/repositories/search_transports_repository_impl.dart';
+import 'package:muvver_app/modules/traveler/domain/usecases/add_traveler.dart';
+import 'package:muvver_app/modules/traveler/external/datasources/mock_sqlite_traveler.dart';
+import 'package:muvver_app/modules/traveler/infra/repositories/add_traveler_repository_impl.dart';
 
 import 'core/constant/route_const.dart';
 import 'core/external/data/mock_sqlite.dart';
@@ -16,6 +19,9 @@ class AppModule extends Module {
       Bind.factory((i) => MockSQLiteTransport(i())),
       Bind.factory((i) => SearchTransportsResitoryImpl(i())),
       Bind.factory((i) => SearchTransportsImpl(i())),
+      Bind.factory((i) => MockSQLiteTraveler(i())),
+      Bind.factory((i) => AddTravelerRepositoryImpl(i())),
+      Bind.factory((i) => AddTravelerImpl(i())),
     ];
   }
 
--- test/modules/app_module_test.dart
+++ test/modules/app_module_test.dart
@@ -4,6 +4,8 @@ import 'package:muvver_app/modules/app_module.dart';
 import 'package:modular_test/modular_test.dart';
 import 'package:muvver_app/modules/transports/domain/unities/transport.dart';
 import 'package:muvver_app/modules/transports/domain/usecase/search_transports.dart';
+import 'package:muvver_app/modules/traveler/domain/usecases/add_traveler.dart';
+import 'package:muvver_app/modules/traveler/infra/models/traveler_model.dart';
 
 void main() {
   initModule(AppModule());
@@ -14,5 +16,11 @@ void main() {
 
     expect(actual, isA<List<Transport>>());
     expect(actual, isNotEmpty);
+  });
+
+  testWidgets('Success - AddTravelerImpl...', (tester) async {
+    final usecase = Modular.get<AddTravelerImpl>();
+
+    usecase(TravelerModel(id: 1, idUser: 1, idTransport: 1));
   });
 }
