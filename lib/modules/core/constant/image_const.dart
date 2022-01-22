2277ae1e67cd3e454cfe0ab3979540a6748bfc0d
Natanael Candido <natanael@sistemaquantum.com.br>
2022-01-22 08:55:32 -0300

    added transport icons

--- lib/modules/core/constant/image_const.dart
+++ lib/modules/core/constant/image_const.dart
@@ -3,14 +3,34 @@
 class ImageConst {
   static const _ROOT = "lib/images/";
 
-/* ------------------------------------------------------- FOLDER HOME */
+/* ---------------------------------------------------- FOLDER HOME ----------*/
   static const BOX_1 = _ROOT + "home/box_1.png";
   static const BOX_2 = _ROOT + "home/box_2.png";
   static const PROFILE = _ROOT + "home/profile.png";
   static const TRUCK_1 = _ROOT + "home/truck_1.png";
   static const TRUCK_2 = _ROOT + "home/truck_2.png";
 
-/* ------------------------------------------------------- FOLDER LOGO */
+/* ---------------------------------------------------- FOLDER LOGO ----------*/
   static const LOGO_1 = _ROOT + "logo/logo_1.png";
   static const LOGO_2 = _ROOT + "logo/logo_2.png";
+
+/* -- -------------------------------------------------- FOLDER TRANSPORT -----*/
+  static const IC_AIRPLANE_1 = _ROOT + "transports/ic_airplane_1.png";
+  static const IC_AIRPLANE_2 = _ROOT + "transports/ic_airplane_2.png";
+  static const IC_BIKE_1 = _ROOT + "transports/ic_bike_1.png";
+  static const IC_BIKE_2 = _ROOT + "transports/ic_bike_2.png";
+  static const IC_BOAT_1 = _ROOT + "transports/ic_boat_1.png";
+  static const IC_BOAT_2 = _ROOT + "transports/ic_boat_2.png";
+  static const IC_BUS_1 = _ROOT + "transports/ic_bus_1.png";
+  static const IC_BUS_2 = _ROOT + "transports/ic_bus_2.png";
+  static const IC_CAR_1 = _ROOT + "transports/ic_car_1.png";
+  static const IC_CAR_2 = _ROOT + "transports/ic_car_2.png";
+  static const IC_MOTORCYCLE_1 = _ROOT + "transports/ic_motorcycle_1.png";
+  static const IC_MOTORCYCLE_2 = _ROOT + "transports/ic_motorcycle_2.png";
+  static const IC_TRAIN_1 = _ROOT + "transports/ic_train_1.png";
+  static const IC_TRAIN_2 = _ROOT + "transports/ic_train_2.png";
+  static const IC_TRUCK_1 = _ROOT + "transports/ic_truck_1.png";
+  static const IC_TRUCK_2 = _ROOT + "transports/ic_truck_2.png";
+  static const IC_VAN_1 = _ROOT + "transports/ic_van_1.png";
+  static const IC_VAN_2 = _ROOT + "transports/ic_van_2.png";
 }
--- lib/modules/core/external/data/mock_database_transport.dart
+++ lib/modules/core/external/data/mock_database_transport.dart
@@ -1,7 +1,16 @@
+import 'package:muvver_app/modules/core/constant/image_const.dart';
 import 'package:muvver_app/modules/transports/infra/models/transport_model.dart';
 
 class MockDatabaseTransport {
   final models = [
-    TransportModel(id: 1, name: "teste", pathImage: "teste.png"),
+    TransportModel(id: 1, name: "Carro", pathImage: ImageConst.IC_CAR_2),
+    TransportModel(id: 2, name: "Avião", pathImage: ImageConst.IC_AIRPLANE_2),
+    TransportModel(id: 3, name: "Caminhão", pathImage: ImageConst.IC_TRUCK_2),
+    TransportModel(id: 4, name: "Van", pathImage: ImageConst.IC_VAN_2),
+    TransportModel(id: 5, name: "Moto", pathImage: ImageConst.IC_MOTORCYCLE_2),
+    TransportModel(id: 6, name: "Bicicleta", pathImage: ImageConst.IC_BIKE_2),
+    TransportModel(id: 7, name: "Trem", pathImage: ImageConst.IC_TRAIN_2),
+    TransportModel(id: 8, name: "Ônibus", pathImage: ImageConst.IC_BUS_2),
+    TransportModel(id: 9, name: "Embarcação", pathImage: ImageConst.IC_BOAT_2),
   ];
 }
